975e5b0a9b68d128cca2f551424d586e09295e50
JoaquinLiendro <46365594@comunidadort.edu.ar>
2025-09-02 22:38:40 -0300

    Propiedades de Liendro

--- Reserva_C/Reserva_C/Models/Empleado.cs
+++ Reserva_C/Reserva_C/Models/Empleado.cs
@@ -1,6 +1,24 @@
 ﻿namespace Reserva_C.Models
 {
-    public class Empleado
+
+    public class Empleado : Persona 
     {
+        //- UserName
+        //- Nombre
+        //- Apellido
+        //- DNI
+        //- Telefono
+        //- Direccion
+        //- FechaAlta
+        //- Email
+        //- Legajo
+
+        public int Id { get; set; }
+
+        public int Legajo { get; set; } 
+
+
+
+
     }
 }
