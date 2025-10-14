d20f41bf50222c74b0cbc84b8c75effe7cb00dd5
JoaquinLiendro <46365594@comunidadort.edu.ar>
2025-10-14 16:57:10 -0300

    Configuracion de servicio DbContext y agregado de propiedades Clientes y Empleados

--- Reserva_C/Reserva_C/Data/ReservaContext.cs
+++ Reserva_C/Reserva_C/Data/ReservaContext.cs
@@ -1,7 +1,19 @@
-﻿namespace Reserva_C.Data
+﻿using Microsoft.EntityFrameworkCore;
+using Reserva_C.Models;
+
+namespace Reserva_C.Data
 {
-    public class ReservaContext
+    public class ReservaContext : DbContext 
     {
+
+        public ReservaContext(DbContextOptions options): base (options) { 
+        
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Empleado> Empleados { get; set; }
+
 
     }
 }
