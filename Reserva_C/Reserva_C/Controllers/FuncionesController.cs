63d9dd3a0b3595fd8fb9ef89fb60c2ee6a0ac448
MateoBlancoORT <blancomateo2004@gmail.com>
2025-10-15 19:39:27 -0300

    creacion de scaffoldings de los modelos Funcion y Pelicula

---
+++ Reserva_C/Reserva_C/Controllers/FuncionesController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Reserva_C.Data;
+using Reserva_C.Models;
+
+namespace Reserva_C.Controllers
+{
+    public class FuncionesController : Controller
+    {
+        private readonly ReservaContext _context;
+
+        public FuncionesController(ReservaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Funciones
+        public async Task<IActionResult> Index()
+        {
+            var reservaContext = _context.Funciones.Include(f => f.Pelicula).Include(f => f.Sala);
+            return View(await reservaContext.ToListAsync());
+        }
+
+        // GET: Funciones/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var funcion = await _context.Funciones
+                .Include(f => f.Pelicula)
+                .Include(f => f.Sala)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (funcion == null)
+            {
+                return NotFound();
+            }
+
+            return View(funcion);
+        }
+
+        // GET: Funciones/Create
+        public IActionResult Create()
+        {
+            ViewData["PeliculaId"] = new SelectList(_context.Peliculas, "Id", "Descripcion");
+            ViewData["SalaId"] = new SelectList(_context.Set<Sala>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: Funciones/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Fecha,Hora,Descripcion,ButacasDisponibles,Confimrada,SalaId,PeliculaId")] Funcion funcion)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(funcion);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PeliculaId"] = new SelectList(_context.Peliculas, "Id", "Descripcion", funcion.PeliculaId);
+            ViewData["SalaId"] = new SelectList(_context.Set<Sala>(), "Id", "Id", funcion.SalaId);
+            return View(funcion);
+        }
+
+        // GET: Funciones/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var funcion = await _context.Funciones.FindAsync(id);
+            if (funcion == null)
+            {
+                return NotFound();
+            }
+            ViewData["PeliculaId"] = new SelectList(_context.Peliculas, "Id", "Descripcion", funcion.PeliculaId);
+            ViewData["SalaId"] = new SelectList(_context.Set<Sala>(), "Id", "Id", funcion.SalaId);
+            return View(funcion);
+        }
+
+        // POST: Funciones/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Fecha,Hora,Descripcion,ButacasDisponibles,Confimrada,SalaId,PeliculaId")] Funcion funcion)
+        {
+            if (id != funcion.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(funcion);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FuncionExists(funcion.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PeliculaId"] = new SelectList(_context.Peliculas, "Id", "Descripcion", funcion.PeliculaId);
+            ViewData["SalaId"] = new SelectList(_context.Set<Sala>(), "Id", "Id", funcion.SalaId);
+            return View(funcion);
+        }
+
+        // GET: Funciones/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var funcion = await _context.Funciones
+                .Include(f => f.Pelicula)
+                .Include(f => f.Sala)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (funcion == null)
+            {
+                return NotFound();
+            }
+
+            return View(funcion);
+        }
+
+        // POST: Funciones/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var funcion = await _context.Funciones.FindAsync(id);
+            if (funcion != null)
+            {
+                _context.Funciones.Remove(funcion);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FuncionExists(int id)
+        {
+            return _context.Funciones.Any(e => e.Id == id);
+        }
+    }
+}
