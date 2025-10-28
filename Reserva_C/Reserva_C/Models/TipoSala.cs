742b26b71d3ee3933864f1765e849d9724920807
agusboxler <44262207@comunidadort.edu.ar>
2025-10-28 13:32:33 -0300

    Fix Links del Layout

--- Reserva_C/Reserva_C/Models/TipoSala.cs
+++ Reserva_C/Reserva_C/Models/TipoSala.cs
@@ -9,7 +9,7 @@ namespace Reserva_C.Models
         //- Precio
         public int Id { get; set; }
         [Required(ErrorMessage = "{0} es requerido.")]
-        [RegularExpression(@"[a-zA-Z áéíóú]*", ErrorMessage = "{0} solo admite caracteres alfabeticos.")]
+        [RegularExpression(@"[a-zA-Z áéíóú]*", ErrorMessage = "{0} solo admite caracteres alfabeticos.")] // Hay que modificar ésto.
         public string Nombre { get; set; }
         [Required(ErrorMessage = "{0} es requerido.")]
         [Range(1, int.MaxValue, ErrorMessage = "{0} debe ser un número positivo.")]
