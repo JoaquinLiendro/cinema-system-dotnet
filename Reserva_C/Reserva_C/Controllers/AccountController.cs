6d1dbff5d7dc0da4e709759454b93cd225876208
JoaquinLiendro <46365594@comunidadort.edu.ar>
2025-11-09 10:24:08 -0300

    Creacion de RegistroUsuario y AccountController

---
+++ Reserva_C/Reserva_C/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Reserva_C.Models;
+using Reserva_C.Models.ViewModels;
+using System.Threading.Tasks;
+
+namespace Reserva_C.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly UserManager<Persona> _usermanager;
+        private readonly SignInManager<Persona> _signInManager;
+        
+        public AccountController(UserManager<Persona> usermanager,SignInManager<Persona> signInManager) {
+            this._usermanager = usermanager;
+            this._signInManager = signInManager;
+        }
+
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Registrar(RegistroUsuario registroUsuario)
+        {
+            if (ModelState.IsValid)
+            {
+                Cliente nuevoCliente = new Cliente()
+                {
+                    Email = registroUsuario.Email,
+                    UserName = registroUsuario.Email,
+                };
+
+                var resultadoCreate = await _usermanager.CreateAsync(nuevoCliente, registroUsuario.Password);
+
+                if (resultadoCreate.Succeeded) {
+                    
+                    await _signInManager.SignInAsync(nuevoCliente, isPersistent:false);
+                    return RedirectToAction("Index", "Home");
+                }
+                
+                foreach(var error in resultadoCreate.Errors)
+                {
+                    ModelState.AddModelError(string.Empty,error.Description);
+                }
+
+            }
+
+            return View(registroUsuario);
+        }
+    }
+}
---
+++ Reserva_C/Reserva_C/Models/ViewModels/RegistroUsuario.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Reserva_C.Models.ViewModels
+{
+    public class RegistroUsuario
+    {
+        
+
+        [Required(ErrorMessage = "{0} es requerido")]
+        [EmailAddress(ErrorMessage = "Formato {0} inválido.")]
+        [StringLength(30, ErrorMessage = "{0} debe contener menos de {1} caracteres")]
+        public  string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} es requerido")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "{0} es requerido")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmacion Contraseña")]
+        [Compare("Password", ErrorMessage = "La contraseña no coincide")]
+        public string ConfirmacionPassword { get; set; }
+    }
+}
