8a995ad8105de9f94ccfd61d76a87c6a0ce9c297
agusboxler <44262207@comunidadort.edu.ar>
2025-11-18 18:45:17 -0300

    Fix Inputs de las Views

--- Reserva_C/Reserva_C/Models/TipoSala.cs
+++ Reserva_C/Reserva_C/Models/TipoSala.cs
@@ -9,7 +9,8 @@ namespace Reserva_C.Models
         //- Precio
         public int Id { get; set; }
         [Required(ErrorMessage = "{0} es requerido.")]
-        [RegularExpression(@"[a-zA-Z áéíóú]*", ErrorMessage = "{0} solo admite caracteres alfabeticos.")] // Hay que modificar ésto.
+        [RegularExpression(@"[a-zA-Z áéíóú ÁÉÍÓÚ 0-9]*", ErrorMessage = "{0} solo admite caracteres alfanumericos.")]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "el {0} debe estar entre el {2} y el {1}")]
         public string Nombre { get; set; }
         [Required(ErrorMessage = "{0} es requerido.")]
         [Range(1, int.MaxValue, ErrorMessage = "{0} debe ser un número positivo.")]
